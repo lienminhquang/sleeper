32338746e7b7fa2066c37520c5154d9477a374fa
DESKTOP-B2MPR32\minhq <minhquang130698@gmail.com>
2022-01-11 22:46:27 +0700

    aa

--- android/app/src/main/java/com/example/sleeper/MainActivity.java
+++ android/app/src/main/java/com/example/sleeper/MainActivity.java
@@ -11,14 +11,29 @@ import android.os.BatteryManager;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.app.Activity;
+import com.example.sleeper.DeviceAdmin;
 
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "com.example.sleeper/battery";
 
+    static final int RESULT_ENABLE = 1;
+    DevicePolicyManager deviceManger;
+    ComponentName compName;
+
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+
+        compName = new ComponentName(this, DeviceAdmin.class);
+        deviceManger = (DevicePolicyManager)
+                getSystemService(Context.DEVICE_POLICY_SERVICE);
+
         super.configureFlutterEngine(flutterEngine);
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
             (call, result) -> {
@@ -32,7 +47,14 @@ public class MainActivity extends FlutterActivity {
                         result.error("UNAVAILABLE", "Batter level not available", null);
                     }
                 }
+                else if (call.method.equals("powerOff")) {
+                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName);
+                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "You should enable the app!");
+                    startActivityForResult(intent, RESULT_ENABLE);
+                }
                 else {
+                    
                     result.notImplemented();
                 }
             }
@@ -52,5 +74,17 @@ public class MainActivity extends FlutterActivity {
             batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1));
         }
         return batteryLevel;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RESULT_ENABLE:
+                if (resultCode == Activity.RESULT_OK) {
+                    deviceManger.lockNow();
+                }
+                return;
+        }
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:background_fetch/background_fetch.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+const platform = MethodChannel('com.example.sleeper/battery');
+
+void backgroundFetchHeadlessTask(HeadlessTask task) async {
+  String taskId = task.taskId;
+  bool isTimeout = task.timeout;
+  if (isTimeout) {
+    // This task has exceeded its allowed running-time.
+    // You must stop what you're doing and immediately .finish(taskId)
+    print("[BackgroundFetch] Headless task timed-out: $taskId");
+    BackgroundFetch.finish(taskId);
+    return;
+  }
+  print('[BackgroundFetch] Headless event received.');
+  // Do your work here...
+  BackgroundFetch.finish(taskId);
 }
 
 class MyApp extends StatelessWidget {
@@ -30,9 +52,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static const platform = MethodChannel('com.example.sleeper/battery');
-
   String _batteryLevel = 'Unknown';
+  String _currentTime = '';
   Future<void> _getBatteryLevel() async {
     String level = '';
     try {
@@ -46,6 +67,72 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Future<void> initPlatformState() async {
+    // Configure BackgroundFetch.
+    int status = await BackgroundFetch.configure(
+        BackgroundFetchConfig(
+            minimumFetchInterval: 15,
+            stopOnTerminate: false,
+            enableHeadless: true,
+            requiresBatteryNotLow: false,
+            requiresCharging: false,
+            requiresStorageNotLow: false,
+            requiresDeviceIdle: false,
+            requiredNetworkType: NetworkType.NONE), (String taskId) async {
+      // <-- Event handler
+      // This is the fetch-event callback.
+      print("[BackgroundFetch] Event received $taskId");
+      setState(() {
+        _events.insert(0, new DateTime.now());
+      });
+      // IMPORTANT:  You must signal completion of your task or the OS can punish your app
+      // for taking too long in the background.
+      BackgroundFetch.finish(taskId);
+    }, (String taskId) async {
+      // <-- Task timeout handler.
+      // This task has exceeded its allowed running-time.  You must stop what you're doing and immediately .finish(taskId)
+      print("[BackgroundFetch] TASK TIMEOUT taskId: $taskId");
+      BackgroundFetch.finish(taskId);
+    });
+    print('[BackgroundFetch] configure success: $status');
+    setState(() {
+      _status = status;
+    });
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(const Duration(seconds: 1), (a) => _getTime());
+    Timer.periodic(const Duration(seconds: 3), (a) => _getBatteryLevel());
+  }
+
+  void _getTime() {
+    setState(() {
+      _currentTime = _formatDateTime(DateTime.now());
+    });
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    return DateFormat('hh:mm:ss').format(dateTime);
+  }
+
+  Future<void> _turnOffScreen() async {
+    String level = '';
+    try {
+      final result = await platform.invokeMethod('powerOff');
+      log('$result');
+    } on PlatformException catch (e, s) {
+      log(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,13 +140,25 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(_batteryLevel),
-            TextButton(
-              child: Text('Get battery level'),
-              onPressed: () {
-                _getBatteryLevel();
-              },
+            Text(
+              _batteryLevel,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              _currentTime,
+              textAlign: TextAlign.center,
+            ),
+            Center(
+              child: TextButton(
+                child: const Text(
+                  'Start',
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () {},
+              ),
             ),
           ],
         ));
